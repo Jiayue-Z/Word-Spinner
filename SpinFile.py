ef950a4a7818599c78417635e48f26a569054b22
Jiayue-Z <zhangjiayue2004@hotmail.com>
2024-02-19 16:28:19 -0700

    Make the file lower case and remove punctuation

--- SpinFile.py
+++ SpinFile.py
@@ -1,10 +1,13 @@
 #A4: Word Spinner, Jay Zhang, https://github.com/Jiayue-Z/Word-Spinner.git
 import Spinner
+import string
 def main():
-    with open("test-synonyms.txt") as file:
-            text = file.read()
+    with open("Essay.txt") as file:
+            read = file.read()
+            lower = read.lower()
+            text = lower.translate(str.maketrans('', '', string.punctuation))
     synonym = Spinner.Spinner()
-    synonym.replace()
+    synonym.replace(text)
 
 if __name__ == '__main__':
     main()
